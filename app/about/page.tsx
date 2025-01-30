2d5f462ec9810fa04c44b0b31bd9972e40d0c057
Angelique <angelique.copere@gmail.com>
2025-01-30 16:26:05 +0100

    add

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex flex-col gap-8">
+            <main className="flex flex-col gap-7">
                 <AboutSection />
                 <ContactSection />
             </main>
